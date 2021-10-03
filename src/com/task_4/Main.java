9575515222fb2fa779dfb3dc83c7a46077f8380f
ZaneProjuma <zaneprojuma@gmail.com>
2021-10-03 22:03:27 +0300

    ATM//Boolean command

---
+++ src/com/task_4/Main.java
@@ -0,0 +1,22 @@
+package com.task_4;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Vai Jus velaties iznemt 20 EUR?");
+        boolean esvelosiznemt20EUR = true;
+        boolean esnevelosiznemt20EUR = false;
+        if (esvelosiznemt20EUR) ;
+        {
+            System.out.print("Ja, velos");
+        }
+        if (esnevelosiznemt20EUR) ;
+        {
+            System.out.print("Ne, nevelos");
+        }
+    }
+    }
